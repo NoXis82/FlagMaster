486b435f48714cf306eb6031943b7f360840116b
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-04-28 23:15:55 +0300

    ui countries content

--- app/src/main/java/ru/noxis/flagmaster/presentation/guide/screen/СountriesContent.kt
+++ app/src/main/java/ru/noxis/flagmaster/presentation/guide/screen/СountriesContent.kt
@@ -10,26 +10,35 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.keyframes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerScope
+import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ru.noxis.core.domain.CountryInfo
 import ru.noxis.flagmaster.presentation.guide.viewmodel.СountriesViewModel
@@ -38,28 +47,89 @@ import ru.noxis.flagmaster.utils.lerp
 import kotlin.math.absoluteValue
 
 @Composable
-fun CountriesContent() {
+fun CountriesContent(modifier: Modifier = Modifier) {
     val viewModel: СountriesViewModel = hiltViewModel()
     val countries by viewModel.countries.collectAsState(emptyList())
 
-    val pagerState = rememberPagerState(pageCount = { countries.size })
+    val horizontalPagerState = rememberPagerState(pageCount = { countries.size })
+    val verticalPagerState = rememberPagerState(pageCount = { countries.size })
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-    HorizontalPager(
-        state = pagerState,
-        modifier = Modifier.padding(vertical = 8.dp)
-    ) { currentPage ->
+        HorizontalPager(
+            state = horizontalPagerState,
+            modifier = Modifier
+                .weight(.7f)
+                .padding(top = 32.dp),
+        ) { currentPage ->
 
-        val pageOffset = pagerState.currentPageOffset(currentPage)
-        val cardAlpha = lerp(0.4f, 1f, 1f - pageOffset.absoluteValue.coerceIn(0f, 1f))
-        val cardRotationY = lerp(0f, 40f, pageOffset.coerceIn(-1f, 1f))
-        val cardScale = lerp(0.5f, 1f, 1f - pageOffset.absoluteValue.coerceIn(0f, 1f))
+            val pageOffset = horizontalPagerState.currentPageOffset(currentPage)
+            val cardAlpha = lerp(0.4f, 1f, 1f - pageOffset.absoluteValue.coerceIn(0f, 1f))
+            val cardRotationY = lerp(0f, 40f, pageOffset.coerceIn(-1f, 1f))
+            val cardScale = lerp(0.5f, 1f, 1f - pageOffset.absoluteValue.coerceIn(0f, 1f))
 
-        CountryItemView(
-            country = countries[currentPage],
-            cardScale = cardScale,
-            cardAlpha = cardAlpha,
-            cardRotationY = cardRotationY,
-            currentPage = currentPage
+            CountryItemView(
+                country = countries[currentPage],
+                cardScale = cardScale,
+                cardAlpha = cardAlpha,
+                cardRotationY = cardRotationY,
+                currentPage = currentPage
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(.3f),
+            contentAlignment = Alignment.Center
+        ) {
+            VerticalPager(
+                state = verticalPagerState,
+                modifier = Modifier.height(72.dp),
+                userScrollEnabled = false,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) { page ->
+                CountryInfoView(countries[page])
+            }
+
+            LaunchedEffect(Unit) {
+                snapshotFlow {
+                    Pair(
+                        horizontalPagerState.currentPage,
+                        horizontalPagerState.currentPageOffsetFraction
+                    )
+                }.collect { (page, offset) ->
+                    verticalPagerState.scrollToPage(page, offset)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun PagerScope.CountryInfoView(
+    country: CountryInfo
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = country.nameCountry.asString(),
+            style = MaterialTheme.typography.titleLarge.copy(
+                fontWeight = FontWeight.Thin,
+                fontSize = 28.sp,
+            )
+        )
+        Text(
+            text = country.capitalCountry.asString(),
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
+                color = Color.Black.copy(alpha = .56f),
+            )
         )
     }
 }
@@ -89,8 +159,7 @@ private fun PagerScope.CountryItemView(
         contentAlignment = Alignment.Center
     ) {
         Box(
-            modifier =
-            Modifier
+            modifier = Modifier
                 .fillMaxWidth()
                 .graphicsLayer {
                     rotationY = cardRotationY
@@ -99,7 +168,8 @@ private fun PagerScope.CountryItemView(
                     scaleX = cardScale
                     scaleY = cardScale
                 }
-                .aspectRatio(0.8f)
+                .aspectRatio(0.8f),
+            contentAlignment = Alignment.Center
         ) {
             Box(
                 modifier = Modifier
@@ -113,7 +183,6 @@ private fun PagerScope.CountryItemView(
 //                    )
             )
             Column {
-                Text(text = country.nameCountry.asString())
                 Image(
                     painter = country.flagIcon.asPainter(),
                     contentDescription = "Shoe Image",
